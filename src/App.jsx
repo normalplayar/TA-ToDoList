1425cd6b6b146023d2a241fc6298fafa247fb2bc
normalplayar <chrissh37@gmail.com>
2025-03-14 18:12:51 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import { TodoWrapper } from './components/TodoWrapper'
-import './App.css'
+import { TodoWrapper } from './components/TodoWrapper';
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export const Todo = ({task, toggleComplete, deleteToDo, editToDo}) => {
     return (
